daf4021302d360db07bb0f102090e5880946609a
rt-heroku <rt@heroku.com>
2019-05-01 14:51:06 -0400

    Update WorkshopApplication.java

--- src/main/java/com/heroku/workshop/WorkshopApplication.java
+++ src/main/java/com/heroku/workshop/WorkshopApplication.java
@@ -2,7 +2,7 @@ package com.heroku.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Added a comment to create a pull request
 @SpringBootApplication
 public class WorkshopApplication {
 
